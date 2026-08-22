584d4884470c5a86a07fdb32dd273e5b3ce75079
taliyanaryan934-creator <taliyanaryan934@gmail.com>
2026-08-22 16:18:04 +0530

    Create prog1_day1.c
    sum of two numbers

---
+++ prog1_day1.c
@@ -0,0 +1,28 @@
+# include<stdio.h>
+
+int main() {
+  int a,b;
+
+  printf ("enter a\n");
+  scanf ("%d", &a);
+
+  printf ("enter b\n");
+  scanf ("%d", &b);
+
+  printf ("sum is :%d", a+b);
+    return 0;
+}
+
+
+
+
+
+
+
+PS F:\The ultimate c programme> gcc prog1_day1.c
+PS F:\The ultimate c programme> ./a.exe    
+enter a
+2
+enter b
+5
+sum is :7
